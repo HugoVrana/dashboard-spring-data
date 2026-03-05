aaf37ac4c9a189d42eeb19923f7bbedf12608535
HugoVrana <hugo.vrana@togolive.de>
2026-03-05 20:28:43 +0100

    test error, double encoding

--- src/main/java/com/dashboard/dataTransferObject/revenue/RevenueRead.java
+++ src/main/java/com/dashboard/dataTransferObject/revenue/RevenueRead.java
@@ -1,5 +1,6 @@
 package com.dashboard.dataTransferObject.revenue;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -7,5 +8,6 @@ import java.math.BigDecimal;
 public class RevenueRead {
     private String id;
     private String month;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT)
     private BigDecimal revenue;
 }
